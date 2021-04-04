9a51df6b27e6a440f78a7f3f95f426700108eb3c
amehahabtu <ameha.addis@gmail.com>
2021-04-04 01:00:26 -0700

    'Rock_Paper_Scissors_ChilrenGame'

---
+++ PythonDemos/Rock_Paper_Scissor.py
@@ -0,0 +1,43 @@
+''' 
+Date: 4/4/2021
+Pogrammer Name:  Ameha Habtu
+Description: This program is developed for my beloved daughters Eldana age 6 , and Danial age 4. 
+It asks the user(palyer) name ,how many times the player wants to keep playing, it tells how many plays left.
+It also informs the result for each play. In the next verision it will include the time spent duration on each play 
+and total time spent, and #will have also many fun and useful features.
+'''
+def rock_paper_scissor():    
+    import random
+    system_choice=random.choice(["Scissors","Rock","Paper"])
+    print()
+    print(f'You have left with {counter} plays.')
+    user_choice= input("Do you want Rock, Paper, or Scissors?\nEnter R for Rock, P for Paper, S for Scissors: ").upper()
+    if (system_choice == user_choice):
+        print('TIE')
+    elif (user_choice== "R" and system_choice=="Scissors" ):
+        print(f'Congratulation {name} You WIN, you said {user_choice} whereas the system had {system_choice}') 
+        #counter += 3         
+        #print(f'You got {3} plays as a bonus.')                                               
+    elif (user_choice== "P" and system_choice=="Rock" ):
+        print(f'Congratulation {name} You WIN, you said {user_choice} whereas the system had {system_choice}') 
+        #counter += 3   
+        #print(f'You got {3} plays as a bonus.')  
+    elif (user_choice== "S" and system_choice=="Paper" ):
+        print(f'Congratulation {name} You WIN, you said {user_choice} whereas the system had {system_choice}') 
+        #counter += 3   
+        #print(f'You got {3} plays as a bonus.')  
+    else:     
+        print(f'Sorry {name} You LOSE, the system WINS , you said {user_choice} whereas the system had {system_choice}')
+def main():
+    global name,counter
+    print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    name=input("Enter your name : ").title()    
+    print(f'Welcome {name} to ROCK-PAPER-SCISSORS game!')
+    num_of_trials=int(input("How many times you want to play? "))
+    counter=num_of_trials
+    for i in range(num_of_trials):
+        rock_paper_scissor()
+        counter -= 1
+    print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    print("Thank you for playing ROCK-PAPER-SCISSORS game in our gaming platform!")
+main()
---
+++ modules.py
@@ -0,0 +1,18 @@
+from math import *
+#from math import pi,tau,sqrt,pow,ceil,floor
+from math import pi as PIE
+import random 
+print(PIE)
+print(pi)
+print(sqrt(9))
+print(e)
+print(tau)
+print(pow(2,3))
+print(2**3)
+print(floor(9.81))#9
+print(floor(9.18)) #9
+print(round(9.81))#10
+print(round(9.18)) #9
+print(ceil(9.81)) #10
+print(ceil(9.18))  #10
+print(random.randint(1,10))
